dc2974c6305fe4df090aaccee0bfc819c24b322a
Weedanta <baguswedanta17@gmail.com>
2025-04-27 15:08:24 +0700

    feat: add template color

---
+++ lib/themes/color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const Color color01 = Color(0xFF292526);
+const Color color02 = Color(0xFF787676);
+const Color color03 = Color(0xFFA3A1A2);
+const Color color04 = Color(0xFFF2F2F2);
+const Color color05 = Color(0xFF121111);
