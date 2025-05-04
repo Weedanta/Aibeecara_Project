304c3f2e6a4274b4f1bef7c71d191136ab0443ae
Weedanta <baguswedanta17@gmail.com>
2025-05-05 04:37:51 +0700

    Update: Color Template

--- lib/themes/color.dart
+++ lib/themes/color.dart
@@ -1,7 +1,10 @@
 import 'dart:ui';
 
-const Color color01 = Color(0xFF292526);
-const Color color02 = Color(0xFF787676);
-const Color color03 = Color(0xFFA3A1A2);
-const Color color04 = Color(0xFFF2F2F2);
-const Color color05 = Color(0xFF121111);
+class ColorStyle {
+  static Color color01 = Color(0xFF292526);
+  static Color color02 = Color(0xFF787676);
+  static Color color03 = Color(0xFFA3A1A2);
+  static Color color04 = Color(0xFFF2F2F2);
+  static Color color05 = Color(0xFF121111);
+  static Color color06 = Color(0xFFFF7F00);
+}
