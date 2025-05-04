5c2a9ba18d8ccf431b190348e0865af831afecc0
Weedanta <baguswedanta17@gmail.com>
2025-05-05 05:09:40 +0700

    fix: onBoarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_project/page/home.dart';
+import 'package:my_project/page/onboarding.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,13 +9,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
-      home: Home(),
+      home: Onboarding(),
     );
   }
 }
