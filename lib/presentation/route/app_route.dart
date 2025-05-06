002180a9500618934517aee8648fbecc98ce8948
Weedanta <baguswedanta17@gmail.com>
2025-05-07 01:56:07 +0700

    feat: add app_route

---
+++ lib/presentation/route/app_route.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import '../screens/splash_screen/splash_screen.dart';
+import '../screens/auth_screen/onboarding.dart';
+import '../screens/auth_screen/login_screen.dart';
+
+class AppRoutes {
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String login = '/login';
+  static const String home = '/home';
+  static const String productDetail = '/product-detail';
+  static const String cart = '/cart';
+  static const String profile = '/profile';
+  
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case splash:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case onboarding:
+        return MaterialPageRoute(builder: (_) => const OnboardingScreen());
+      case login:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
