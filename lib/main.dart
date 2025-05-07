e5359f40d5affa0986952150809abd041beaacd5
Weedanta <baguswedanta17@gmail.com>
2025-05-07 19:49:56 +0700

    refactor: update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_project/di/container.dart';
+import 'package:my_project/presentation/bloc/auth/auth_bloc.dart';
+import 'package:my_project/presentation/bloc/cart/cart_bloc.dart';
+import 'package:my_project/presentation/bloc/product/product_bloc.dart';
 import 'package:my_project/presentation/route/app_route.dart';
 import 'package:my_project/presentation/themes/theme_data.dart';
 
 void main() {
-
   WidgetsFlutterBinding.ensureInitialized();
   
   // Set preferred orientations
@@ -22,7 +26,10 @@ void main() {
       systemNavigationBarIconBrightness: Brightness.dark,
     ),
   );
-
+  
+  // Setup dependency injection
+  setupDependencies();
+  
   runApp(const MyApp());
 }
 
@@ -31,13 +38,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FakeStore',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      initialRoute: AppRoutes.splash,
-      onGenerateRoute: AppRoutes.generateRoute,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (context) => getIt<AuthBloc>(),
+        ),
+        BlocProvider<ProductBloc>(
+          create: (context) => getIt<ProductBloc>(),
+        ),
+        BlocProvider<CartBloc>(
+          create: (context) => getIt<CartBloc>(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'FakeStore',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        initialRoute: AppRoutes.splash,
+        onGenerateRoute: AppRoutes.generateRoute,
+      ),
     );
   }
 }
-
