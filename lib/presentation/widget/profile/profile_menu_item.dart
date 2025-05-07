fb9cd4d72d39821072e878d1f96d483bdbdc2d19
Weedanta <baguswedanta17@gmail.com>
2025-05-07 19:48:51 +0700

    feat: add profile wigdet

---
+++ lib/presentation/widget/profile/profile_menu_item.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:my_project/presentation/themes/color.dart';
+
+class ProfileMenuItem extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final VoidCallback onTap;
+  final Widget? trailing;
+
+  const ProfileMenuItem({
+    Key? key,
+    required this.icon,
+    required this.title,
+    required this.onTap,
+    this.trailing,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(12),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        child: Row(
+          children: [
+            // Leading Icon
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                color: AppColors.primary.withOpacity(0.1),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Icon(
+                icon,
+                size: 20,
+                color: AppColors.primary,
+              ),
+            ),
+            const SizedBox(width: 16),
+            
+            // Title
+            Expanded(
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: AppColors.textPrimary,
+                ),
+              ),
+            ),
+            
+            // Trailing Widget or Arrow
+            trailing ??
+                Icon(
+                  Icons.arrow_forward_ios,
+                  size: 16,
+                  color: AppColors.textLight,
+                ),
+          ],
+        ),
+      ),
+    );
+  }
+}
